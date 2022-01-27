4d2a6127c3bd5306937a9963406d47494cb61d76
Biel Simon Rojas <bielsimon2@gmail.com>
2022-01-27 14:13:03 +0100

    Added AppStore.ts

---
+++ src/renderer/core/AppStore.ts
@@ -0,0 +1,28 @@
+import {container, inject, singleton} from "tsyringe";
+import {makeAutoObservable, makeObservable, observable} from "mobx";
+
+export let useAppStore = () => {
+    return container.resolve(AppStore);
+};
+
+@singleton()
+export class AppStore {
+    @observable
+    projects: any[] = [
+        {id: '1',
+            open: [
+                {path: 'C:\\Users\\biel\\projects\\bis\\bis-admin'}
+            ]
+        }
+    ]
+    @observable
+    counter = 0
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    openWS(){
+        let path = `C:\\Users\\biel\\projects\\bis\\bis-admin`
+
+    }
+}
--- src/renderer/views/HomeView.tsx
+++ src/renderer/views/HomeView.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {useAppStore} from "@/renderer/core/AppStore";
 
 export default function HomeView() {
+    let store = useAppStore()
+    let counter = store.counter
     return (
         <div>
             <h1>Home</h1>
             <p>This is the initial view.</p>
+            <p>{counter} <button onClick={() => store.counter++}>Hello</button></p>
         </div>
     );
 }
